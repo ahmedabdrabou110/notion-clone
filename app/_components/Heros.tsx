30355ab18c0f1bbb51da69f3a0c7f7bc9a77f444
ahmedabdrabou110 <a.abdrabou1102000@icloud.com>
2023-10-16 14:12:46 +0300

    creating Heros Components

--- app/_components/Heros.tsx
+++ app/_components/Heros.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
 import React from "react";
 
 const Heros = () => {
-  return <div>Heros</div>;
+  return (
+    <div className="flex flex-col justify-center items-center max-w-5xl">
+      <div className="flex items-center">
+        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+          <Image
+            src="/documents.png"
+            alt="documents photo"
+            fill
+            className="object-contain"
+          />
+        </div>
+        <div className="relative h-[400px] w-[400px] hidden  md:block">
+          <Image
+            src="/reading.png"
+            alt="reading photo"
+            fill
+            className="object-contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Heros;
