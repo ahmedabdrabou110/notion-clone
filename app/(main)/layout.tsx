9e59c8a4aeb803113974980d138329db159e0452
ahmedabdrabou110 <a.abdrabou1102000@icloud.com>
2023-10-19 01:56:31 +0300

    create sidebar of document page and create document page

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Spinner from "@/components/Spinner";
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+import Navigation from "./_components/Navigation";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  const { isLoading, isAuthenticated } = useConvexAuth();
+
+  if (isLoading) {
+    return (
+      <div className="w-full mt-2 flex justify-center items-center">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="flex h-full dark:bg-[#1f1f1f]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto overflow-x-hidden">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default layout;
