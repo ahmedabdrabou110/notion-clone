9b3d94b2612cc749680db148424cc91f1e7d588b
ahmedabdrabou110 <a.abdrabou1102000@icloud.com>
2023-11-06 13:13:35 +0200

    create Toolbar (icon | title)

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,3 +1,4 @@
+import { useMutation } from "convex/react";
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
@@ -255,66 +256,93 @@ export const getSearch = query({
 });
 
 export const getById = query({
-  args:{documentId: v.id("documents")},
-  handler : async (ctx , args) =>{
+  args: { documentId: v.id("documents") },
+  handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
-    if(!identity) {
-      throw new Error ("Not authenticated")
+    if (!identity) {
+      throw new Error("Not authenticated");
     }
 
     const document = await ctx.db.get(args.documentId);
 
-    if(!document) {
-      throw new Error ("Not Found");
-    }
-
-    if(document.isPublised && !document.isArchived) {
-      return document ;
-    }
-
-    const userId = identity.subject ;
-
-    if(document.userId !== userId) {
-      throw new Error ("Not Authorized");
-    }
-
-    return document; 
-  }
-});
-
-
-export const update = mutation({
-  args:  {
-    id :v.id("documents"),
-    title:v.optional(v.string()),
-    content:v.optional(v.string()),
-    coverImage:v.optional(v.string()),
-    icon:v.optional(v.string()),
-    isPublished:v.optional(v.boolean()),
-  } ,
-  handler:async (ctx, args) =>{
-    const identity = await ctx.auth.getUserIdentity();
-    if(!identity) {
-      throw new Error("Not Authentication");
-    }
-    const userId = identity.subject ; 
-    
-    const { id , ...rest } = args;
-    
-    const existingDocument = await ctx.db.get(args.id);
-
-    if(!existingDocument) {
+    if (!document) {
       throw new Error("Not Found");
     }
 
-    if(existingDocument?.userId !== userId) {
+    if (document.isPublised && !document.isArchived) {
+      return document;
+    }
+
+    const userId = identity.subject;
+
+    if (document.userId !== userId) {
+      throw new Error("Not Authorized");
+    }
+
+    return document;
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("documents"),
+    title: v.optional(v.string()),
+    content: v.optional(v.string()),
+    coverImage: v.optional(v.string()),
+    icon: v.optional(v.string()),
+    isPublished: v.optional(v.boolean()),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Not Authentication");
+    }
+    const userId = identity.subject;
+
+    const { id, ...rest } = args;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Not Found");
+    }
+
+    if (existingDocument?.userId !== userId) {
       throw new Error("unAuthorized");
     }
 
-    const document = await ctx.db.patch(args.id , {
-      ...rest
-    })
+    const document = await ctx.db.patch(args.id, {
+      ...rest,
+    });
 
-    return document ;
-  }
-})
+    return document;
+  },
+});
+
+export const removeIcon = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Not Authentication");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocuments = await ctx.db.get(args.id);
+
+    if (!existingDocuments) {
+      throw new Error("Not Found");
+    }
+
+    if (existingDocuments.userId !== userId) {
+      throw new Error("NotAutherized");
+    }
+
+    const documents = await ctx.db.patch(args.id, {
+      icon: undefined,
+    });
+
+    return documents;
+  },
+});
